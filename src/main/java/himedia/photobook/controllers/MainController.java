cd7a119d21e65858793a5df694ebd2f4f7d61fc3
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-08 11:59:03 +0900

    개발 브런치 활성화용도 커밋

--- src/main/java/himedia/photobook/controllers/MainController.java
+++ src/main/java/himedia/photobook/controllers/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
+	//
 	@RequestMapping({"/"})
 	public String welcome() {
 		return "/WEB-INF/views/index/index.jsp";
