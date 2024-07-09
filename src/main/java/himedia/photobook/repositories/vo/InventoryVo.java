b96f63570d32e337f39be7481211f85d16f75a6d
yooseungwonn <163090539+yooseungwonn@users.noreply.github.com>
2024-07-09 11:49:31 +0900

    24. 7. 9 오전 10시

---
+++ src/main/java/himedia/photobook/repositories/vo/InventoryVo.java
@@ -0,0 +1,49 @@
+package himedia.photobook.repositories.vo;
+
+// 작성자 : 유승원
+public class InventoryVo {
+	private String albumId;
+	private Long albumPrice;
+	private String aQuantity;
+	
+	public InventoryVo() {
+		
+	}
+
+	public InventoryVo(String albumId,Long albumPrice, String aQuantity) {
+		super();
+		this.albumId = albumId;
+		this.albumPrice = albumPrice;
+		this.aQuantity = aQuantity;
+	}
+
+	public String getAlbumId() {
+		return albumId;
+	}
+
+	public void setAlbumId(String albumId) {
+		this.albumId = albumId;
+	}
+
+	public Long getAlbumPrice() {
+		return albumPrice;
+	}
+
+	public void setAlbumPrice(Long albumPrice) {
+		this.albumPrice = albumPrice;
+	}
+
+	public String getaQuantity() {
+		return aQuantity;
+	}
+
+	public void setaQuantity(String aQuantity) {
+		this.aQuantity = aQuantity;
+	}
+
+	@Override
+	public String toString() {
+		return "InventoryVo [albumId=" + albumId + ", albumPrice=" + albumPrice + ", aQuantity=" + aQuantity + "]";
+	}
+	
+}
