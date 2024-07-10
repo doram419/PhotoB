40c58cbaff29f754b17b580d376952e65b199c2c
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-10 11:44:49 +0900

    고객 관리 페이지.jsp 복사 작성 후 어드민 컨트롤러에서 연결 및 헤더 변경

--- src/main/java/himedia/photobook/controllers/AdminController.java
+++ src/main/java/himedia/photobook/controllers/AdminController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping({"/admin"})
 public class AdminController {
-	@RequestMapping({"/admin", "/admin/"})
-	public String adminHome() {
+	@RequestMapping({"/home"})
+	public String home() {
 		return "/WEB-INF/views/admin/admin-pages.jsp";
 	}
 	
-	
+	@RequestMapping({"/customerManagement", "customerManage", "cm"})
+	public String customerManagement() {
+		return "/WEB-INF/views/admin/customer-management-page.jsp";
+	}
 }
