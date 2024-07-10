7945706fc4aca797614b22ad1f527c3ee4007efe
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-10 12:15:40 +0900

    admin_header.jsp 경로 수정 및 AdminController.java::home 매핑 수정

--- src/main/java/himedia/photobook/controllers/AdminController.java
+++ src/main/java/himedia/photobook/controllers/AdminController.java
@@ -6,22 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping({"/admin"})
 public class AdminController {
-	@RequestMapping({"/home"})
+	@RequestMapping({"","/home"})
 	public String home() {
 		return "/WEB-INF/views/admin/admin-pages.jsp";
 	}
 	
-	@RequestMapping({"/customerManagement", "customerManage", "cm"})
+	@RequestMapping({"/customerManagement", "/customerManage", "/cm"})
 	public String customerManagement() {
 		return "/WEB-INF/views/admin/customer-management-page.jsp";
 	}
 	
-	@RequestMapping({"/orderManagement", "orderManage", "om"})
+	@RequestMapping({"/orderManagement", "/orderManage", "/om"})
 	public String orderManagement() {
 		return "/WEB-INF/views/admin/order-management-page.jsp";
 	}
 	
-	@RequestMapping({"/customerService", "cs"})
+	@RequestMapping({"/customerService", "/cs"})
 	public String customerService() {
 		return "/WEB-INF/views/admin/customer-service-page.jsp";
 	}
@@ -29,7 +29,7 @@ public class AdminController {
 	/**
 	 * db로 줄이지 않은 이유는 data base의 약자와 겹치기 때문
 	 * */
-	@RequestMapping({"/dashboard", "dash"})
+	@RequestMapping({"/dashboard", "/dash"})
 	public String dashboard() {
 		return "/WEB-INF/views/admin/dashboard-page.jsp";
 	}
