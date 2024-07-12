e676a6c14e426d5ab7c1c1f08efe0fb83bd2abbf
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-12 15:45:27 +0900

    Revert "뷰 리졸버 추가 작업 중"
    This reverts commit 1df3a3977e343806feaa1a2d82ab7d20a61a4430.

--- src/main/java/himedia/photobook/controllers/AdminController.java
+++ src/main/java/himedia/photobook/controllers/AdminController.java
@@ -10,22 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
 	@RequestMapping({"","/home"})
 	public String home() {
-		return "admin/admin_pages";
+		return "/WEB-INF/views/admin/admin_pages.jsp";
 	}
 	
 	@RequestMapping({"/customerManagement", "/customerManage", "/cm"})
 	public String customerManagement() {
-		return "admin/admin_customer_management";
+		return "/WEB-INF/views/admin/admin_customer_management.jsp";
 	}
 	
 	@RequestMapping({"/orderManagement", "/orderManage", "/om"})
 	public String orderManagement() {
-		return "admin/admin_order_management";
+		return "/WEB-INF/views/admin/admin_order_management.jsp";
 	}
 	
 	@RequestMapping({"/customerService", "/cs"})
 	public String customerService() {
-		return "admin/admin_customer_service";
+		return "/WEB-INF/views/admin/admin_customer_service.jsp";
 	}
 	
 	/**
@@ -33,7 +33,7 @@ public class AdminController {
 	 * */
 	@RequestMapping({"/dashboard", "/dash"})
 	public String dashboard() {
-		return "admin/admin_dashboard";
+		return "/WEB-INF/views/admin/admin_dashboard.jsp";
 	}
 
 	@RequestMapping("/refund")
