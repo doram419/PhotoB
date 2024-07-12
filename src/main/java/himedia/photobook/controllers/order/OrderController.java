cc1ecec80747fc7883c0847a990088b1f9d1e286
hyowonzzang <163090718+hyowonzzang@users.noreply.github.com>
2024-07-12 18:00:05 +0900

    간단한수정
    간단수정

--- src/main/java/himedia/photobook/controllers/order/OrderController.java
+++ src/main/java/himedia/photobook/controllers/order/OrderController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({ "/order" })
+@RequestMapping({ "/users/order" })
 public class OrderController {
 	@RequestMapping({"/",""})
 	public String order() {
