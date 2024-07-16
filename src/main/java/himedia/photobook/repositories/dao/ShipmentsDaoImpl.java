cd8b3a27686903e8466ab03e536b4d99297738f6
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-16 12:05:09 +0900

    관리자:배송 조회에 필요한 "배송정보"를 끌어오기 위해  관련 mapper와 dao에 코드 추가

--- src/main/java/himedia/photobook/repositories/dao/ShipmentsDaoImpl.java
+++ src/main/java/himedia/photobook/repositories/dao/ShipmentsDaoImpl.java
@@ -9,7 +9,17 @@ public class ShipmentsDaoImpl {
 	@Autowired
 	private SqlSession session;
 	
+	/**
+	 * orderId로 배송정보 테이블에서 "주문 상태(Shipment_Status)" 정보를 받아오는 메서드
+	 * */
 	public String selectStatusByOrderID(String orderId) {
 		return session.selectOne("shipments.selectStatusByOrderID", orderId);
+	}
+	
+	/**
+	 * orderId로 주문 상태 정보를 받아오는 메서드
+	 * */
+	public String selectShipmentInfoByOrderID(String orderId) {
+		return session.selectOne("shipments.selectShipmentInfoByOrderID", orderId);
 	}
 }
--- src/main/java/himedia/photobook/services/admin/AdminDeliveryServiceImpl.java
+++ src/main/java/himedia/photobook/services/admin/AdminDeliveryServiceImpl.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import himedia.photobook.repositories.dao.OrdersDaoImpl;
+import himedia.photobook.repositories.dao.ShipmentsDaoImpl;
 import himedia.photobook.repositories.dao.UsersDaoImpl;
 import himedia.photobook.repositories.vo.OrdersVo;
+import himedia.photobook.repositories.vo.ShipmentsVo;
 import himedia.photobook.repositories.vo.UsersVo;
 
 @Service("adminDeliveryService")
@@ -19,6 +21,8 @@ public class AdminDeliveryServiceImpl {
 	private OrdersDaoImpl orderDao;
 	@Autowired
 	private UsersDaoImpl userDao;
+	@Autowired
+	private ShipmentsDaoImpl shipmentsDao;
 	
 	/**
 	 * 배송 관리에 필요한 정보들을 모두 리턴해주는 함수
@@ -27,13 +31,15 @@ public class AdminDeliveryServiceImpl {
 		List<Map<String, Object>> deliveryInfoList = new ArrayList<Map<String, Object>>();
 		Map<String, Object> deliveryInfos = null;
 		UsersVo usersVo = null;
+		ShipmentsVo shipmentsVo = null;  
 		
 		List<OrdersVo> orderList = orderDao.selectAllOrders();
 		
 		for (OrdersVo ordersVo : orderList) {
 			deliveryInfos = new HashMap<String, Object>();	
 			usersVo = userDao.selectUserByUserId(ordersVo.getUserId());
-			
+//			shipmentsVo = shipmentsDao.selectStatusByOrderID(null);
+					
 			deliveryInfos.put("ordersVo", ordersVo);
 			deliveryInfos.put("usersVo", usersVo);
 			
