5f4eedc179d5a2581b7d3e0e70168c3f1dfe65cb
JJiwonn <163959530+JJiwonn@users.noreply.github.com>
2024-07-19 06:58:26 +0900

    Update AlbumDaoImpl.java

--- src/main/java/himedia/photobook/repositories/dao/AlbumDaoImpl.java
+++ src/main/java/himedia/photobook/repositories/dao/AlbumDaoImpl.java
@@ -47,7 +47,7 @@ public class AlbumDaoImpl implements AlbumDao {
 	
 	@Override
     public List<AlbumVo> findAll() {
-		return sqlSession.selectList("adlum.findAll");
+		return sqlSession.selectList("album.findAll");
 	}
 	
 	// 앨범 수정
