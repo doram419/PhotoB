35c6c078c2d2c7b44dbf5c3d63389e098f28ebe1
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-07-20 10:41:04 +0900

    conflict 해결, 구동 확인은 못 함

--- src/main/java/himedia/photobook/repositories/dao/RefundDaoImpl.java
+++ src/main/java/himedia/photobook/repositories/dao/RefundDaoImpl.java
@@ -1,15 +1,23 @@
 package himedia.photobook.repositories.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-@Repository("refundDao")
-public class RefundDaoImpl {
+import himedia.photobook.repositories.vo.RefundVo;
+
+@Repository
+public class RefundDaoImpl implements RefundDao{
 	@Autowired
 	private SqlSession session;
 	
 	public String selectStatusByOrderID(String orderId) {
 		return session.selectOne("refund.selectStatusByOrderID", orderId);
+	}
+	
+	public List<RefundVo> selectAllRefunds(){
+		return session.selectList("refund.selectAllRefunds");
 	}
 }
---
+++ src/main/java/himedia/photobook/repositories/vo/BoardVo.java
@@ -0,0 +1,114 @@
+package himedia.photobook.repositories.vo;
+
+import java.util.Date;
+
+public class BoardVo {
+	private Long boardId;
+	private String title;
+	private String content;
+	private Date regDate;
+	private String userId;
+	private String status;
+	
+	
+	
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public BoardVo() {
+
+	}
+
+	
+	public BoardVo(String title, String content, String userId) {
+		this.title = title;
+		this.content = content;
+	}
+
+
+
+	public BoardVo(Long boardId, String title, String content, Date regDate, String userId, String userName) {
+		super();
+		this.boardId = boardId;
+		this.title = title;
+		this.content = content;
+		this.regDate = regDate;
+	}
+
+
+	public Long getBoardId() {
+		return boardId;
+	}
+
+
+	public void setBoardId(Long boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+	
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVo [boardId=" + boardId + ", title=" + title + ", content=" + content + ", regDate=" + regDate
+				+ "]";
+	}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+}
