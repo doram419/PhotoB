6325745aea64f90646584cd79acfa9420c36cc9d
dlwldnjsald <163959530+JJiwonn@users.noreply.github.com>
2024-07-31 10:06:31 +0900

    컨플릭트 수정
    123

--- src/main/webapp/javascript/admin/admin.js
+++ src/main/webapp/javascript/admin/admin.js
@@ -80,7 +80,6 @@
 		}
 	}
 
-<<<<<<< HEAD
 // 환불 완료
 function adminRefund(event) {
 		event.preventDefault();
@@ -118,10 +117,7 @@ function deleteProduct(event) {
 			window.location.href = event.currentTarget.href;
 		}
 	}
-=======
 
 
-});
 
 
->>>>>>> cb27a4a109eb85d3e1bf5e8ce221a140e7d84ad9
